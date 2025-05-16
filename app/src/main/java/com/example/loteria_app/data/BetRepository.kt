55f779df801269df4d0720bf56b195fe2034c8d0
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-05-16 09:54:40 -0500

    feat: criando camada de repository

---
+++ app/src/main/java/com/example/loteria_app/data/BetRepository.kt
@@ -0,0 +1,24 @@
+package com.example.loteria_app.data
+
+class BetRepository(private val dao: BetDao) {
+
+    companion object {
+
+        private var instance: BetRepository? = null
+
+        fun getInstance(betDao: BetDao) {
+            instance ?: BetRepository(betDao).also {
+                instance = it
+            }
+        }
+    }
+
+    fun getBets(type: String): List<Bet> {
+        return dao.getNumbersByType(type)
+    }
+
+    fun insertBet(bet: Bet) {
+        dao.insert(bet)
+    }
+
+}
